9cde0025743f76fa88090262670b29ec4d687897
Negar Sakhaei <negar.sakhaei@yahoo.com>
2020-01-06 00:05:02 +0330

    fixed styling

--- RDS.py
+++ RDS.py
@@ -1,48 +1,60 @@
 import numpy as np
 
+
 class RDS:
     def __init__(self, n, inf, sup, LowerB):
         self.n = n
         self.assignment = np.zeros(n)
-        self.temp_assignment = np.zeros(n)
+        self.temp_assignment = [-1] * n
         self.rds = np.zeres(n + 1)
         self.inf = inf
         self.sup = sup
         self.LowerBound = LowerB
+
     def DFBB(self, lbi, ubi, i):
         lb = inf
         ub = ubi
         v = i
         s = False
         current = 0
+
         def depth():
             if v == n:
                 s = True
                 ub = lb
                 self.assignment = self.temp_assignment
-                if lbi < ub: width()
-                else: end()
+                if lbi < ub:
+                    width()
+                else:
+                    end()
+
         def width():
             if A[v] == d:
                 v = v - 1
-                if v == i: end()
-                else: width()
+                if v == i:
+                    end()
+                else:
+                    width()
             else:
                 self.temp_assignment[v] += 1
                 lb = LowerBound(i, v)
-                if lb < ub: depth()
-                else: width()
+                if lb < ub:
+                    depth()
+                else:
+                    width()
+
         def end():
-            if s: 
+            if s:
                 current = ub
-            else: 
+            else:
                 current = 'F'
-        
+
         depth()
         return current
+
     def RDS(self, ubi):
         rds[self.n] = inf
-        for i in range(n-1, 0, -1):
+        for i in range(n - 1, 0, -1):
             lbp = rds[i + 1]
             ubp = UPPERBOUND(ubi, lbp, i)
             ub = self.DFBB(lbp, ubp, i)
@@ -51,6 +63,11 @@ class RDS:
             else:
                 return 'Fail'
         return ub
+
     def UPPERBOUND(ubi, lbp, i):
         return 99999999999
-        
+
+
+def LowerBound(A, i, v, C):
+    lb1 = 
+
