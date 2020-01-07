3d0afcba75655ef825af627ba7e13be1bfbc6039
Negar Sakhaei <negar.sakhaei@yahoo.com>
2020-01-07 06:30:37 +0330

    added example

---
+++ ex.py
@@ -0,0 +1,31 @@
+from RDS import RDS
+
+very_large_number = 999999999999999
+
+def op_eq_mod2(a, b):
+    return a == (b % 2)
+
+
+def op_ge_mod10(a, b):
+    return a >= b
+
+
+
+C = [
+    {'Constraint': [1, 0, 0, 0, 0, 0, 0, 0], 'Result': (op_eq_mod2, 0), 'Valuation': 1},
+    {'Constraint': [0, 1, 0, 0, 0, 0, 0, 0], 'Result': (op_eq_mod2, 0), 'Valuation': 1},
+    {'Constraint': [0, 0, 1, 0, 0, 0, 0, 0], 'Result': (op_eq_mod2, 0), 'Valuation': 1},
+    {'Constraint': [0, 0, 0, 1, 0, 0, 0, 0], 'Result': (op_eq_mod2, 0), 'Valuation': 1},
+    {'Constraint': [0, 0, 0, 0, 1, 0, 0, 0], 'Result': (op_eq_mod2, 0), 'Valuation': 1},
+    {'Constraint': [0, 0, 0, 0, 0, 1, 0, 0], 'Result': (op_eq_mod2, 0), 'Valuation': 1},
+    {'Constraint': [0, 0, 0, 0, 0, 0, 1, 0], 'Result': (op_eq_mod2, 0), 'Valuation': 1},
+    {'Constraint': [0, 0, 0, 0, 0, 0, 0, 1], 'Result': (op_eq_mod2, 0), 'Valuation': 1},
+    {'Constraint': [0, 0, 0, 0, 1, 1, 1, 1], 'Result': (op_eq_mod2, 0), 'Valuation': very_large_number},
+    {'Constraint': [0, 1, 0, 1, 0, 1, 0, 1], 'Result': (op_eq_mod2, 0), 'Valuation': very_large_number},
+    {'Constraint': [0, 0, 1, 1, 0, 0, 1, 1], 'Result': (op_eq_mod2, 0), 'Valuation': very_large_number},
+    {'Constraint': [1, 1, 1, 1, 1, 1, 1, 1], 'Result': (op_eq_mod2, 0), 'Valuation': very_large_number},
+    {'Constraint': [1, 1, 1, 1, 1, 1, 1, 1], 'Result': (op_ge_mod10, 1), 'Valuation': very_large_number},
+]
+
+solve = RDS(8, 0, very_large_number, C)
+print(solve.RDS(very_large_number))
