4b88494f8f31fa3c3a3492e069ae9c3a94ca4dd9
mahyaj98 <jamshidianm98@gmail.com>
2020-01-07 16:28:17 +0330

    totally PEP8 compatible

--- RDS.py
+++ RDS.py
@@ -4,58 +4,61 @@ very_large_number = 999999999999999
 
 
 class RDS:
-    def __init__(self, n, inf, sup, C):
+    def __init__(self, n, inf, sup, c):
         self.n = n
         self.assignment = np.zeros(n)
         self.temp_assignment = [-1] * n
         self.rds = np.zeros(n + 1)
         self.inf = inf
         self.sup = sup
-        self.C = C
+        self.C = c
 
     def dfbb(self, lbi, ubi, i):
-        lb = self.inf
-        ub = ubi
-        v = i
-        s = False
-        current = 0
+
+        values = {
+            'lb': self.inf,
+            'ub': ubi,
+            'v': i,
+            's': False,
+            'current': 0
+        }
 
         def depth():
-            if v == self.n:
-                s = True
-                ub = lb
+            if values['v'] == self.n:
+                values['s'] = True
+                values['ub'] = values['lb']
                 self.assignment = self.temp_assignment
-                if lbi < ub:
+                if lbi < values['ub']:
                     width()
                 else:
                     end()
             else:
-                self.temp_assignment[v] = self.sup - 1
-                v = v + 1
+                self.temp_assignment[values['v']] = self.sup - 1
+                values['v'] = values['v'] + 1
 
         def width():
-            if self.temp_assignment[v] == 1:
-                v = v - 1
-                if v == i:
+            if self.temp_assignment[values['v']] == 1:
+                values['v'] = values['v'] - 1
+                if values['v'] == i:
                     end()
                 else:
                     width()
             else:
-                self.temp_assignment[v] += 1
-                lb = self.lower_bound(i, v)
-                if lb < ub:
+                self.temp_assignment[values['v']] += 1
+                values['lb'] = self.lower_bound(i, values['v'])
+                if values['lb'] < values['ub']:
                     depth()
                 else:
                     width()
 
         def end():
-            if s:
-                current = ub
+            if values['s']:
+                values['current'] = values['ub']
             else:
-                current = 'F'
+                values['current'] = 'F'
 
         depth()
-        return current
+        return values['current']
 
     def rds_function(self, ubi):
         self.rds[self.n] = self.inf
@@ -70,13 +73,13 @@ class RDS:
         return ub
 
     @staticmethod
-    def upper_bound( ubi, lbp, i):
+    def upper_bound(ubi, lbp, i):
         # TODO: Change the definition
         return very_large_number
 
     @staticmethod
-    def all_var_in_cons( constraint, vars):
-        return np.any(np.subtract(constraint, vars) == -1)
+    def all_var_in_cons(constraint, assigned_vars):
+        return np.any(np.subtract(constraint, assigned_vars) == -1)
 
     def lower_bound(self, i, v):
         assigned_vars = np.array([1 if j in range(i, v) else 0 for j in range(self.n)])
@@ -84,13 +87,14 @@ class RDS:
         lb_bc = 0
         for c in self.C:
             if self.all_var_in_cons(c, assigned_vars):
-                if not c['Result'][0](np.sum(np.multiply(np.array(c['Constraint']), self.temp_assignment)), c['Result'][1]):
+                if not c['Result'][0](np.sum(np.multiply(np.array(c['Constraint']),
+                                                         self.temp_assignment)), c['Result'][1]):
                     lb_bc += c['Valuation']
 
         # LB_fc
         lb_fc = 0
         for c in self.C:
-            for v in (list(set(range(self.n)) - set(range(i ,v)))):
+            for v in (list(set(range(self.n)) - set(range(i, v)))):
                 tmp = assigned_vars
                 tmp[v] = 1
                 if not self.all_var_in_cons(c, assigned_vars) and self.all_var_in_cons(c, tmp):
